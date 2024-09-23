ddb2ef04dfdcfb88f130683c535902d83eed6d3e
mohamedumar07 <76449096+mohamedumar07@users.noreply.github.com>
2024-09-23 21:11:05 +0530

    Time: 539 ms (73.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -0,0 +1,33 @@
+# Write your MySQL query statement below
+#LENGTH() returns the length of a string
+
+SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select tweet_id from Tweets where LENGTH(content) > 15
