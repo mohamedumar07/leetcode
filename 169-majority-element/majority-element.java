4ad9d47af1003a1daf2276b9c3d59ce1c5c6e7a3
mohamedumar07 <76449096+mohamedumar07@users.noreply.github.com>
2025-02-08 18:18:06 +0530

    Time: 1 ms (99.81%) | Memory: 53.2 MB (48.92%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,28 +1,68 @@
 import java.lang.Math;
 import java.util.HashMap;
 class Solution {
-    HashMap<Integer, Integer> map = new HashMap<>();
+    
+    //*SOLUTION - 1
+
+    // HashMap<Integer, Integer> map = new HashMap<>();
     public int majorityElement(int[] nums) {
-        int threshold = (int)Math.ceil(nums.length/2);
-        int majority = 0;
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(map.containsKey(nums[i]))
-            {
-                map.put(nums[i], map.get(nums[i])+1);
-            }
-            else{
-                map.put(nums[i], 1);
+    //     int threshold = (int)Math.ceil(nums.length/2);
+    //     int majority = 0;
+    //     for(int i = 0; i < nums.length; i++)
+    //     {
+    //         if(map.containsKey(nums[i]))
+    //         {
+    //             map.put(nums[i], map.get(nums[i])+1);
+    //         }
+    //         else{
+    //             map.put(nums[i], 1);
+    //         }
+    //     }
+    //     for(int i : map.keySet())
+    //     {
+    //         if(map.get(i) > threshold)
+    //         {
+    //             majority = i;
+    //             break;
+    //         }
+    //     }
+    //     return majority;
+    int candidate = nums[0];
+    int vote = 1;
+
+    for(int i = 1; i < nums.length; i++)
+    {
+        if(nums[i] == candidate){
+            vote++;
+        }else{
+            vote--;
+            if(vote < 0){
+                candidate = nums[i];
+                vote = 1;
             }
         }
-        for(int i : map.keySet())
-        {
-            if(map.get(i) > threshold)
-            {
-                majority = i;
-                break;
-            }
-        }
-        return majority;
+    }
+
+    return candidate;
+
+    // //SOLUTION - 2
+    // int count = 0;
+    // int element = 0;
+    // for(int num : nums)
+    // {
+    //     if(count == 0)
+    //     {
+    //         element = num;
+    //         count = 1;
+    //     }
+    //     else if(element == num)
+    //     {
+    //         count++;
+    //     }
+    //     else{
+    //         count--;
+    //     }
+    // }
+    // return element;
     }
 }
