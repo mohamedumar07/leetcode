3e3db251b3193a83bf9851f0050da6f2d9734e2a
mohamedumar07 <76449096+mohamedumar07@users.noreply.github.com>
2025-08-03 08:26:03 +0530

    Time: 1 ms (92.42%), Space: 46.3 MB (5.47%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -6,19 +6,31 @@ class Solution {
         sumLeft[0] = 0;
         sumRight[n - 1] =0;
 
-        for(int i = 1; i < n; i++){
-            sumLeft[i] = sumLeft[i - 1] + nums[i - 1];
-        }
-
         for(int i = n - 2; i >= 0; i--){
             sumRight[i] = sumRight[i + 1] + nums[i + 1];
         }
 
-        for(int j = 0; j < n; j++){
-            if(sumRight[j] == sumLeft[j]){
-                return j;
+        // for(int i = 1; i < n; i++){
+        //     sumLeft[i] = sumLeft[i - 1] + nums[i - 1];
+        // }
+
+        int leftSum = 0;
+        for(int i = 0; i < n; i++){
+            if(sumRight[i] == leftSum){
+                return i;
             }
+            leftSum += nums[i];
         }
+
+        // for(int i = n - 2; i >= 0; i--){
+        //     sumRight[i] = sumRight[i + 1] + nums[i + 1];
+        // }
+
+        // for(int j = 0; j < n; j++){
+        //     if(sumRight[j] == sumLeft[j]){
+        //         return j;
+        //     }
+        // }
 
         return -1;
     }
