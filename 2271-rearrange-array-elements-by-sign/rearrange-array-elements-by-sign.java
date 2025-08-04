c77bb5f9aafe2cf8d3961457e5c5f22da1bcd7a1
mohamedumar07 <76449096+mohamedumar07@users.noreply.github.com>
2025-08-04 23:46:22 +0530

    Time: 3 ms (100.00%) | Memory: 77 MB (67.50%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,47 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int size = nums.length;
-
-        int ans[] = new int[nums.length];
-
+        int n = nums.length;
+        int result[] = new int [n];
         int pos = 0;
-
         int neg = 1;
-
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > 0){
-                ans[pos] = nums[i];
+        for(int num: nums){
+            if(num > 0){
+                result[pos] = num; 
                 pos += 2;
             }else{
-                ans[neg] = nums[i];
+                result[neg] = num;
                 neg += 2;
             }
         }
-        // int size = nums.length;
-        // int pos[] = new int[size/2];
-        // int neg[] = new int[size/2];
-        // int retArr[] = new int[size];
-        // int posc = 0;
-        // int negc = 0;
-        // int count = 0;
-        // for(int i = 0; i < size; i++)
-        // {
-        //     if(nums[i] > 0){
-        //         pos[posc] = nums[i];
-        //         posc++;
-        //     }
-        //     else{
-        //         neg[negc] = nums[i];
-        //         negc++;
-        //     }
-        // }
-        // for(int j = 0; j < size; j = j + 2)
-        // {
-        //     retArr[j] = pos[count];
-        //     retArr[j+1] = neg[count];
-        //     count++;
-        // }
-        // return retArr;
-        return ans;
+        return result;
     }
 }
